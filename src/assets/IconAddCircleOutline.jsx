411334787e4870930e6feb2c0e22429d2669cfdf
Ravi Potes Mangra <potesmangrar11@gmail.com>
2024-06-12 18:34:31 -0500

    First commit Note-React-App

---
+++ src/assets/IconAddCircleOutline.jsx
@@ -0,0 +1,30 @@
+// icon:add-circle-outline | Ionicons https://ionicons.com/ | Ionic Framework
+function IconAddCircleOutline(props) {
+  return (
+    <svg
+      viewBox="0 0 512 512"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeMiterlimit={10}
+        strokeWidth={32}
+        d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z"
+      />
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={32}
+        d="M256 176v160M336 256H176"
+      />
+    </svg>
+  );
+}
+
+export default IconAddCircleOutline;
